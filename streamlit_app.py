ed5e5ddc6ba857c72ced57aaa7bf50919b709f61
mjangamas <125757647+mjangamas@users.noreply.github.com>
2023-02-20 23:19:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
-streamlit.write('The user entered ', fruit_choice)
+## streamlit.write('The user entered ', fruit_choice)
 
 ## New section to display fruitvice api response
 
